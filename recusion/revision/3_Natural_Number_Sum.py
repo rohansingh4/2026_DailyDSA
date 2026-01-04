267af653b308e62f571c58f49bb4191fded7f532
rohansingh4 <rohan.singh1104@gmail.com>
2026-01-04 11:07:10 +0530

    palindrom string

--- recusion/revision/3_Natural_Number_Sum.py
+++ recusion/revision/3_Natural_Number_Sum.py
@@ -10,3 +10,14 @@ Output : 15
 Explanation : 1 + 2 + 3 + 4 + 5 = 15
 
 '''
+
+def calcSum(n):
+    if n < 1:
+        return 0
+
+    return (n + calcSum(n-1))
+
+
+n = 3
+x = calcSum(n)
+print(x)
---
+++ recusion/revision/4_palindrome.py
@@ -0,0 +1,24 @@
+'''
+Docstring for recusion.revision.4_palindrome
+
+Input : malayalam
+Output : Yes
+Reverse of malayalam is also
+malayalam.
+
+Input : max
+Output : No
+Reverse of max is not max. 
+
+'''
+
+def palin(s):
+  if len(s) == 1:
+    return True
+  if s[0] == s[-1]:
+    return palin(s[1:-1])
+  else :
+    return False  
+
+s = "aba"
+print(palin(s))
